c36d97367a82bca77769697fbfaed1699d3c7018
Paul Geppert <Paul.Geppert@Student.HPI.de>
2016-06-07 22:47:04 +0200

    Updated fetch-merge .bat-script, to handle 64bit-Git too.

--- fetch_merge.bat
+++ fetch_merge.bat
@@ -1,3 +1,4 @@
-"C:\Program Files (x86)\Git\bin\git" fetch
-"C:\Program Files (x86)\Git\bin\git" merge --ff-only origin/team09
+if exist %windir%\SysWOW64\ ("C:\Program Files\Git\bin\git" fetch
+"C:\Program Files\Git\bin\git" merge --ff-only origin/team09) else ("C:\Program Files (x86)\Git\bin\git" fetch
+"C:\Program Files (x86)\Git\bin\git" merge --ff-only origin/team09)
 @pause
