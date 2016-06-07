46d57ef73e20e5be4bb99843b2b53373897fc71a
pascal <pascal.fuehrlich@student.hpi.de>
2016-06-08 00:52:38 +0200

    skript fix

--- fetch_merge.bat
+++ fetch_merge.bat
@@ -1,4 +1,3 @@
-if exist %windir%\SysWOW64\ ("C:\Program Files\Git\bin\git" fetch
-"C:\Program Files\Git\bin\git" merge --ff-only origin/team09) else ("C:\Program Files (x86)\Git\bin\git" fetch
-"C:\Program Files (x86)\Git\bin\git" merge --ff-only origin/team09)
+"C:\Program Files (x86)\Git\bin\git" fetch
+"C:\Program Files (x86)\Git\bin\git" merge --ff-only origin/team09
 @pause
